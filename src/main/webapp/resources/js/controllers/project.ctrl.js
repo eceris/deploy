ed025906abb1ee8f0d61b365080ca06a7feacf4f
eceris <eceris@daou.co.kr>
2016-11-17 13:31:32 +0900

    패키지 삭제시 패키지 목록 리로딩 하도록 변경

--- src/main/webapp/resources/js/controllers/project.ctrl.js
+++ src/main/webapp/resources/js/controllers/project.ctrl.js
@@ -14,79 +14,9 @@ app.controller('ProjectCtrl', function($scope, $http, $modal, projectFactory, $l
 			ctrl.isLoading = false;
 		});
 	};
-
-	$scope.buildProject = function(project) {
-		$modal.open({
-			templateUrl : '/resources/partials/dialogView/projectSuccess.html',
-			size : 'lg',
-			controller : 'ModalController',
-			resolve : {
-				data : function() {
-					return;
-				}
-			}
-		});
-		projectFactory.buildProject(project);
-	};
-
-	$scope.scanCustomFile = function(project) {
-		$modal.open({
-			templateUrl : '/resources/partials/dialogView/projectSuccess.html',
-			size : 'lg',
-			controller : 'ModalController',
-			resolve : {
-				data : function() {
-					return;
-				}
-			}
-		});
-		projectFactory.scanCustomFile(project);
-	};
-
-	$scope.buildStandardProject = function(project) {
-
-		var modalInstance = $modal.open({
-			templateUrl : '/resources/partials/dialogView/buildStandardProject.html',
-			controller : 'projectModalCtrl',
-			size : 'lg',
-			resolve : {
-				project : function() {
-					return project;
-				}
-			}
-		});
-		modalInstance.result.then(function() {});
-	};
-
-	$scope.mobileBuild = function(project) {
-		projectFactory.mobileBuild(project);
-	};
-
 });
 
-app.controller('projectModalCtrl', function($scope, $modalInstance, projectFactory, project, $modal) {
-	$scope.project = project;
-
-	$scope.buildStandardProject = function(version, branch) {
-		if (!(version || 0)) {
-			alert('DO 버전을 입력하세요.')
-			return;
-		}
-		if (!(branch || 0)) {
-			alert('브랜치 명을 입력하세요.')
-			return;
-		}
-		projectFactory.buildStandardProject($scope.project, version, branch);
-	};
-	$scope.ok = function() {
-		$modalInstance.close();
-	};
-	$scope.cancel = function() {
-		$modalInstance.dismiss('cancel');
-	};
-});
-
-app.controller('projectDetailController', function($scope, $routeParams, projectDetailFactory) {
+app.controller('projectDetailController', function($q, $scope, $routeParams, projectDetailFactory) {
 	var id = $routeParams.id;
 	$scope.id = $routeParams.id;
 	$scope.ctrl = this;
@@ -95,22 +25,22 @@ app.controller('projectDetailController', function($scope, $routeParams, project
 		$scope.get();
 		$scope.getPacakges();
 	};
+	
 	$scope.getPacakges = function() {
 		$scope.ctrl.displayed = [];
-		$scope.ctrl.callServer = function callServer(tableState) {
+		$scope.ctrl.callServer = function callServer(params) {
 			$scope.ctrl.isLoading = true;
-			var pagination = tableState.pagination;
+			$scope.ctrl.params = params;
+			var pagination = params.pagination;
 			var start = pagination.start || 0;
 			var number = pagination.number || 10;
-			projectDetailFactory.getFiles(id, start, number, tableState).then(function(result) {
+			projectDetailFactory.getFiles(id, start, number, params).then(function(result) {
 				$scope.ctrl.displayed = result.data;
-				tableState.pagination.numberOfPages = result.numberOfPages;
+				params.pagination.numberOfPages = result.numberOfPages;
 				$scope.ctrl.isLoading = false;
 			});
 		};
 	};
-	
-		
 
 	$scope.get = function() {
 		projectDetailFactory.get(id).success(function(data, status, headers, config) {
@@ -122,10 +52,18 @@ app.controller('projectDetailController', function($scope, $routeParams, project
 	};
 
 	$scope.delete = function(id) {
+		var deferred = $q.defer();
 		projectDetailFactory.delete(id).success(function(data, status, headers, config) {
 			console.log(data);
+			deferred.resolve();
 		}).error(function(data, status, headers, config) {
 			console.log(status);
+		});
+		
+		deferred.promise.then(function() {
+			console.log("done");
+			$scope.ctrl.displayed = [];
+			$scope.ctrl.callServer($scope.ctrl.params);
 		});
 	};
 
@@ -135,8 +73,9 @@ app.controller('projectDetailController', function($scope, $routeParams, project
 	$scope.build = function() {
 		projectDetailFactory.build($scope.id).then(function() {
 			console.log("done");
-			$scope.ctrl = {};
-			$scope.getPacakges();
+			$scope.ctrl.displayed = [];
+            $scope.ctrl.callServer($scope.ctrl.params);
+            alert('빌드완료! 목록에서 패키지를 확인하세요.');
 		});
 	};
 	$scope.standardbuild = function() {
@@ -146,8 +85,9 @@ app.controller('projectDetailController', function($scope, $routeParams, project
 	$scope.checksource = function() {
 		projectDetailFactory.checksource($scope.id).then(function() {
 			console.log("done");
-			$scope.ctrl = {};
-			$scope.getPacakges();
+			$scope.ctrl.displayed = [];
+			$scope.ctrl.callServer($scope.ctrl.params);
+			alert('소스 비교 완료! 로그에서 에러를 확인하세요.');
 		});
 	};
 	$scope.mobile = function() {
--- src/main/webapp/resources/js/services/project.serv.js
+++ src/main/webapp/resources/js/services/project.serv.js
@@ -25,52 +25,6 @@ app.factory('projectFactory', function($q, $filter, $timeout, $http, cfpLoadingB
 		return deferred.promise;
 	};
 
-
-	projectFactory.buildStandardProject = function(prj, version, branch) {
-		$('#buildBtn').hide();
-		var logEl = $('#log').find('pre');
-		logEl.css('display', 'block');
-		var xhttp = new XMLHttpRequest();
-		var length = 0;
-		// version, branch, maild_path, maild_version, site
-		var params = "version=" + version + "&branch=" + branch + "&maild_path=&maild_version=&site=" + prj.path;
-		xhttp.open("POST", '/project/build/standard', true);
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.onreadystatechange = function() {
-			if (xhttp.readyState == 2) {
-				cfpLoadingBar.start();
-			}
-			if (xhttp.readyState > 2 && xhttp.status == 200) {
-				var result = xhttp.responseText.substring(length, xhttp.responseText.length)
-				length = xhttp.responseText.length;
-				logEl.append('<span>' + result + '</span>');
-				$('#log').scrollTop($('#log')[0].scrollHeight);
-			}
-			if (xhttp.readyState == 4) {
-				cfpLoadingBar.complete();
-			}
-		};
-		xhttp.send(params);
-	};
-
-	projectFactory.mobileBuild = function(prj, version) {
-		console.log(prj);
-		console.log(version);
-		// http://172.21.26.191:8000/DOFactory/brandtable?version=2.3.5&companyname=유비온&platform=all
-		// http://172.21.27.40:8000/build/totaldownloads?appName=UBION2.3.5.0&version=2.3.5&platform=iOS
-		var name = prj.name;
-		$http.get('/project/version?project=' + prj.path).success(function(data, status, headers, config) {
-			console.log(data);
-			var version = data.value.substr(0, 5);
-			if (version.length < 1) {
-				version = 'all';
-			}
-			window.open("http://172.21.26.191:8000/DOFactory/brandtable?version=" + version + "&companyname=" + name + "&platform=all", "New Window", "width=950,height=750,resizable=1");
-		}).error(function(data, status, headers, config) {
-			console.log(status);
-		});
-	};
-
 	return projectFactory;
 
 });
